23e8e5090b7aa4e71c75bba4a78e8a759a052beb
Juan Carlos Mendoza <juan.mendoza.ca@tecsup.edu.pe>
2025-08-30 18:47:33 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/main.kt
@@ -0,0 +1,34 @@
+package acuario
+
+
+    fun construirAcuario() {
+        val acuario1 = Acuario()
+        acuario1.imprimirTamano()
+        // altura y largo por defecto
+        val acuario2 = Acuario(ancho = 25)
+        acuario2.imprimirTamano()
+        //ancho por defecto
+        val acuario3 = Acuario(alto = 35, largo = 110)
+        acuario3.imprimirTamano()
+        //todo personalizado
+        val acuario4 = Acuario(ancho = 25, alto = 35, largo = 110)
+        acuario4.imprimirTamano()
+        val acuario6 = Acuario(numeroDePeces = 29)
+        acuario6.imprimirTamano()
+        val acuario7 = Acuario(numeroDePeces = 29)
+            acuario7.imprimirTamano()
+            acuario7.volumen = 70
+            acuario7.imprimirTamano()
+
+        val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+        acuario8.imprimirTamano()
+
+
+
+
+    }
+
+    fun main(){
+        construirAcuario()
+
+    }
